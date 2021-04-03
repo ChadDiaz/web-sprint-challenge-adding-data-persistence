c77f6530293b2fa697e7d9f39ac78739112e8931
ChadDiaz <chadcdiaz@gmail.com>
2021-04-03 01:33:31 -0400

    still setting up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
-// build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet(), express.json());
+
+server.get("/", (req, res, next) => {
+  res.status(200).json({ message: "api up" });
+});
+
+server.use((error, req, res, next) => {
+  const errorStatus = error.status || 500;
+  const errorMessage = error.message || "Server failed...";
+  res.status(errorStatus).json({ message: errorMessage, stack: error.stack });
+});
+
+module.exports = server;
