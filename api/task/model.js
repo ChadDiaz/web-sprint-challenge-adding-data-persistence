26191c6f2005575fc0842d0825b89ec7a3b4d8c2
ChadDiaz <chadcdiaz@gmail.com>
2021-04-03 20:36:16 -0400

    joined tables in tasks

--- api/task/model.js
+++ api/task/model.js
@@ -1,11 +1,22 @@
 const db = require("../../data/dbConfig.js");
 
 async function getTask() {
-  const task = await db("tasks");
-  return task.map((task) => {
+  const tasks = await db
+    .select(
+      "tasks.task_id",
+      "tasks.task_description",
+      "tasks.task_notes",
+      "tasks.task_completed",
+      "projects.project_name",
+      "projects.project_description"
+    )
+    .from("tasks")
+    .join("projects", "tasks.project_id", "=", "projects.project_id");
+
+  return tasks.map((task) => {
     return {
       ...task,
-      task_completed: task.task_completed === 0 ? false : true,
+      task_completed: task.task_completed == 0 ? false : true,
     };
   });
 }
