83c7b96bf64d28e5d784c7e46a8f194c740d1fcc
ChadDiaz <chadcdiaz@gmail.com>
2021-04-03 20:16:17 -0400

    added tasks

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,30 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig.js");
+
+async function getTask() {
+  const task = await db("tasks");
+  return task.map((task) => {
+    return {
+      ...task,
+      task_completed: task.task_completed === 0 ? false : true,
+    };
+  });
+}
+
+async function getTaskById(id) {
+  const [task] = await db("tasks").where({ task_id: id });
+  return {
+    ...task,
+    task_completed: task.task_completed == 0 ? false : true,
+  };
+}
+
+async function addTask(body) {
+  const newId = await db("tasks").insert(body);
+  return getTaskById(newId[0]);
+}
+
+module.exports = {
+  getTask,
+  getTaskById,
+  addTask,
+};
